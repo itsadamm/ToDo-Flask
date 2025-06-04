e82e2b6e91c2b5d3dfc3a4634f65220e5d6e35f6
Adam Adri <adamadri@u.boisestate.edu>
2025-06-04 16:43:02 -0600

    Initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+# sets config variable. Using sqlite database for now.
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+@app.route('/')
+def index():
+    return render_template('base.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
